164c7600f6d7544c95cf4f11963ed2a0008aaae5
Igor Proskurin <igor.v.proskurin@gmail.com>
2024-05-19 22:00:31 -0700

    add tests for rationals

--- tests/eval_test.rs
+++ tests/eval_test.rs
@@ -1,3 +1,5 @@
+use num::{One, Rational64, Zero};
+
 use bernstein::Bernstein;
 
 use crate::utils::equal_with_abs_tol;
@@ -29,4 +31,47 @@ fn eval_second_order_f64() {
     assert!(equal_with_abs_tol(c.eval(0.0), 1.0, DBL_TOL));
     assert!(equal_with_abs_tol(c.eval(1.0), -1.0, DBL_TOL));
     assert!(equal_with_abs_tol(c.eval(0.5), 1.0, DBL_TOL));
+}
+
+#[test]
+fn eval_second_order_rational() {
+    let p1 = Rational64::new(1, 1);
+    let p2 = Rational64::new(2, 1);
+    let p3 = Rational64::new(-1, 1);
+
+    let c: Bernstein<Rational64, Rational64, 3> = Bernstein::new([p1, p2, p3]);
+
+    let u = Rational64::zero();
+    assert_eq!(c.eval(u), Rational64::one());
+
+    let u = Rational64::one();
+    assert_eq!(c.eval(u), Rational64::new(-1, 1));
+
+    let u = Rational64::new(1, 2);
+    assert_eq!(c.eval(u), Rational64::one());
+
+    let u = Rational64::new(1, 17);
+    assert_eq!(c.eval(u), Rational64::new(319, 289));
+}
+
+#[test]
+fn eval_third_order_rational() {
+    let p0 = Rational64::new(1, 5);
+    let p1 = Rational64::new(-3, 7);
+    let p2 = Rational64::new(4, 13);
+    let p3 = Rational64::new(-11, 17);
+
+    let c: Bernstein<Rational64, Rational64, 4> = Bernstein::new([p0, p1, p2, p3]);
+
+    let u = Rational64::new(0, 1);
+    assert_eq!(c.eval(u), p0);
+
+    let u = Rational64::new(1, 1);
+    assert_eq!(c.eval(u), p3);
+
+    let u = Rational64::new(1, 2);
+    assert_eq!(c.eval(u), Rational64::new(-6263, 61880));
+
+    let u = Rational64::new(1, 17);
+    assert_eq!(c.eval(u), Rational64::new(3899827, 38002055));
 }
