7f6eae0acd22e0dcf5c00676f16884a4d473f057
Igor Proskurin <igor.v.proskurin@gmail.com>
2024-05-20 17:52:38 -0700

    add differentiation method with tests

--- src/impls.rs
+++ src/impls.rs
@@ -1,13 +1,15 @@
 //! Implementations of fundamental operations on polynomials in Bernstein basis.
 
+use num::FromPrimitive;
 use num::Num;
-use std::ops::{Add, Mul};
+use std::array;
+use std::ops::{Add, Sub, Mul};
 
 use crate::Bernstein;
 
 impl<T, U, const N: usize> Bernstein<T, U, N> where
-    T: Copy + Add<T, Output = T> + Mul<U, Output = T>,
-    U: Copy + Num,
+    T: Copy + Add<T, Output = T> + Sub<T, Output = T> + Mul<U, Output = T>,
+    U: Copy + Num + FromPrimitive,
 {
 
     /// Evaluate polynomial in the Bernstein basis at the point `0 <= u <= 1`
@@ -25,6 +27,27 @@ impl<T, U, const N: usize> Bernstein<T, U, N> where
             }
         }
         q[0]
+    }
+
+    /// Return new instance that is a derivative of the original polynomial in
+    /// the Bernstein basis.
+    ///
+    /// See Piegl & Tiller. "The NURBS book". Springer Science & Business Media
+    /// (2012) -- p.22, Eq. (1.9).
+    pub fn diff(&self) -> Bernstein<T, U, {N - 1}> where
+        [(); N - 1]:
+    {
+        let coef: [T; N - 1] = array::from_fn(
+            |i| -> T {
+                (self.coef[i + 1] - self.coef[i]) *
+                (U::from_usize(N - 1).unwrap() / (self.segm.1 - self.segm.0))
+            }
+        );
+
+        Bernstein {
+            segm: self.segm,
+            coef: coef,
+        }
     }
 
 }
---
+++ tests/diff_test.rs
@@ -0,0 +1,44 @@
+use num::Complex;
+
+use bernstein::Bernstein;
+
+use crate::utils::equal_with_abs_tol;
+
+mod utils;
+
+#[test]
+fn diff_zero_order() {
+    let c: Bernstein<f64, f64, 1> = Bernstein::new([1.0]);
+    assert_eq!(c.diff().coef().clone(), []);
+}
+
+#[test]
+fn diff_first_order_complex() {
+    const DBL_TOL: f64 = 1.0e-15;
+    let p0 = Complex::new(0.0, 0.0);
+    let p1 = Complex::new(2.0, 1.0);
+
+    let c: Bernstein<Complex<f64>, f64, 2> = Bernstein::new([p0, p1]);
+    let d = c.diff().coef().clone();
+    assert!(equal_with_abs_tol(d[0].re, 2.0, DBL_TOL));
+    assert!(equal_with_abs_tol(d[0].im, 1.0, DBL_TOL));
+}
+
+#[test]
+fn diff_second_order_f64() {
+    const DBL_TOL: f64 = 1.0e-15;
+    let c:Bernstein<f64, f64, 3> = Bernstein::new([1.0, -1.0, 2.0]);
+    let coef = c.diff().coef().clone();
+    assert!(equal_with_abs_tol(coef[0], -4.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[1], 6.0, DBL_TOL));
+}
+
+#[test]
+fn diff_third_order_f64() {
+    const DBL_TOL: f64 = 1.0e-15;
+    let c:Bernstein<f64, f64, 4> = Bernstein::new([1.0, 0.0, 1.0, -1.0]);
+    let coef = c.diff().coef().clone();
+    assert!(equal_with_abs_tol(coef[0], -3.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[1], 3.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[2], -6.0, DBL_TOL));
+}
