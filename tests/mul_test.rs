2845e02fd4c2ee6b3e2a269cacb4ad6ea664720b
Igor Proskurin <igor.v.proskurin@gmail.com>
2024-05-26 08:16:17 -0700

    add multiplication tests

--- tests/mul_test.rs
+++ tests/mul_test.rs
@@ -8,15 +8,55 @@ use crate::utils::equal_with_abs_tol;
 mod utils;
 
 #[test]
-    fn product_fist_order() {
-        const DBL_TOL: f64 = 1.0e-15;
-        let p: Bernstein<f64, f64, 2> = Bernstein::new([0.0, 1.0]);
-        let q: Bernstein<f64, f64, 2> = Bernstein::new([2.0, 0.0]);
+fn product_fist_order() {
+    const DBL_TOL: f64 = 1.0e-15;
+    let p: Bernstein<f64, f64, 2> = Bernstein::new([0.0, 1.0]);
+    let q: Bernstein<f64, f64, 2> = Bernstein::new([2.0, 0.0]);
 
-        let c = p * q;
+    let c = p * q;
 
-        let coef = c.coef().clone();
-        assert!(equal_with_abs_tol(coef[0], 0.0, DBL_TOL));
-        assert!(equal_with_abs_tol(coef[1], 1.0, DBL_TOL));
-        assert!(equal_with_abs_tol(coef[2], 0.0, DBL_TOL));
-    }
+    let coef = c.coef().clone();
+    assert!(equal_with_abs_tol(coef[0], 0.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[1], 1.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[2], 0.0, DBL_TOL));
+
+    let p: Bernstein<f64, f64, 2> = Bernstein::new([0.0, 1.0]);
+    let q: Bernstein<f64, f64, 2> = Bernstein::new([2.0, 0.0]);
+
+    let c = q * p;
+
+    let coef = c.coef().clone();
+    assert!(equal_with_abs_tol(coef[0], 0.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[1], 1.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[2], 0.0, DBL_TOL));
+}
+
+#[test]
+fn product_second_order_by_third_order() {
+    const DBL_TOL: f64 = 1.0e-15;
+    let p: Bernstein<f64, f64, 3> = Bernstein::new([0.0, 2.0, 1.0]);
+    let q: Bernstein<f64, f64, 4> = Bernstein::new([1.0, 2.0, 0.0, 0.0]);
+
+    let c = p * q;
+
+    let coef = c.coef().clone();
+    assert!(equal_with_abs_tol(coef[0], 0.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[1], 0.8, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[2], 2.5, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[3], 0.6, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[4], 0.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[5], 0.0, DBL_TOL));
+
+    let p: Bernstein<f64, f64, 3> = Bernstein::new([0.0, 2.0, 1.0]);
+    let q: Bernstein<f64, f64, 4> = Bernstein::new([1.0, 2.0, 0.0, 0.0]);
+
+    let c = q * p;
+
+    let coef = c.coef().clone();
+    assert!(equal_with_abs_tol(coef[0], 0.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[1], 0.8, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[2], 2.5, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[3], 0.6, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[4], 0.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[5], 0.0, DBL_TOL));
+}
