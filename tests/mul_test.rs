53808f1050d57b2fc216907ccdf806fd96d08888
Igor Proskurin <igor.v.proskurin@gmail.com>
2024-05-26 10:47:05 -0700

    first by second order multiplication test

--- tests/mul_test.rs
+++ tests/mul_test.rs
@@ -32,6 +32,32 @@ fn product_fist_order() {
 }
 
 #[test]
+fn product_first_order_by_second_order() {
+    const DBL_TOL: f64 = 2.0e-15;
+    let p: Bernstein<f64, f64, 2> = Bernstein::new([5.0, 3.0]);
+    let q: Bernstein<f64, f64, 3> = Bernstein::new([1.0, 2.0, 5.0]);
+
+    let c = p * q;
+
+    let coef = c.coef().clone();
+    assert!(equal_with_abs_tol(coef[0], 5.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[1], 23.0 / 3.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[2], 37.0 / 3.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[3], 15.0, DBL_TOL));
+
+    let p: Bernstein<f64, f64, 2> = Bernstein::new([5.0, 3.0]);
+    let q: Bernstein<f64, f64, 3> = Bernstein::new([1.0, 2.0, 5.0]);
+
+    let c = q * p;
+
+    let coef = c.coef().clone();
+    assert!(equal_with_abs_tol(coef[0], 5.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[1], 23.0 / 3.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[2], 37.0 / 3.0, DBL_TOL));
+    assert!(equal_with_abs_tol(coef[3], 15.0, DBL_TOL));
+}
+
+#[test]
 fn product_second_order_by_third_order() {
     const DBL_TOL: f64 = 1.0e-15;
     let p: Bernstein<f64, f64, 3> = Bernstein::new([0.0, 2.0, 1.0]);
